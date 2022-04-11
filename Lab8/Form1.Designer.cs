b9e3137ed4657fbca5cd897a67e92410e1d849e7
Ko1baser <astarikov541@gmail.com>
2022-04-11 23:18:37 +0300

    Добавил listbox, можно создавать новые разделы

--- Lab8/Form1.Designer.cs
+++ Lab8/Form1.Designer.cs
@@ -33,13 +33,18 @@
             this.btnDelete = new System.Windows.Forms.Button();
             this.txtBoxName = new System.Windows.Forms.TextBox();
             this.txtBoxValue = new System.Windows.Forms.TextBox();
+            this.cmbBoxRegistryList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtBox
             // 
-            this.txtBox.Location = new System.Drawing.Point(84, 95);
+            this.txtBox.Location = new System.Drawing.Point(72, 133);
             this.txtBox.Name = "txtBox";
-            this.txtBox.Size = new System.Drawing.Size(100, 23);
+            this.txtBox.Size = new System.Drawing.Size(209, 23);
             this.txtBox.TabIndex = 0;
             // 
             // btnCreate
@@ -63,23 +68,74 @@
             // 
             // txtBoxName
             // 
-            this.txtBoxName.Location = new System.Drawing.Point(84, 134);
+            this.txtBoxName.Location = new System.Drawing.Point(72, 190);
             this.txtBoxName.Name = "txtBoxName";
-            this.txtBoxName.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxName.Size = new System.Drawing.Size(209, 23);
             this.txtBoxName.TabIndex = 3;
             // 
             // txtBoxValue
             // 
-            this.txtBoxValue.Location = new System.Drawing.Point(84, 175);
+            this.txtBoxValue.Location = new System.Drawing.Point(72, 251);
             this.txtBoxValue.Name = "txtBoxValue";
-            this.txtBoxValue.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxValue.Size = new System.Drawing.Size(209, 23);
             this.txtBoxValue.TabIndex = 4;
+            // 
+            // cmbBoxRegistryList
+            // 
+            this.cmbBoxRegistryList.FormattingEnabled = true;
+            this.cmbBoxRegistryList.Location = new System.Drawing.Point(72, 74);
+            this.cmbBoxRegistryList.Name = "cmbBoxRegistryList";
+            this.cmbBoxRegistryList.Size = new System.Drawing.Size(209, 23);
+            this.cmbBoxRegistryList.TabIndex = 5;
+            this.cmbBoxRegistryList.SelectedIndexChanged += new System.EventHandler(this.cmbBoxRegistryList_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Выберите ключ реестра";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Введите имя нового раздела";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Введите имя нового параметра";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(208, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Введите значение нового параметра";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(613, 354);
+            this.ClientSize = new System.Drawing.Size(731, 433);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbBoxRegistryList);
             this.Controls.Add(this.txtBoxValue);
             this.Controls.Add(this.txtBoxName);
             this.Controls.Add(this.btnDelete);
@@ -99,5 +155,10 @@
         private Button btnDelete;
         private TextBox txtBoxName;
         private TextBox txtBoxValue;
+        private ComboBox cmbBoxRegistryList;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
--- Lab8/Form1.cs
+++ Lab8/Form1.cs
@@ -1,5 +1,6 @@
 using Microsoft.Win32;
 
+
 namespace Lab8
 {
     public partial class Form1 : Form
@@ -7,25 +8,84 @@ namespace Lab8
         public Form1()
         {
             InitializeComponent();
+            
+            btnCreate.Enabled = false;
+            btnDelete.Enabled = false;
+            txtBox.Enabled = false;
+            txtBoxName.Enabled = false;
+            txtBoxValue.Enabled = false;
 
 
-
+            List<string> registryList = new List<string>() { "HKEY_CLASSES_ROOT", "HKEY_CURRENT_USER", 
+                "HKEY_LOCAL_MACHINE", "HKEY_USERS", "HKEY_CURRENT_CONFIG" };
+            cmbBoxRegistryList.Items.AddRange(registryList.ToArray());
 
         }
 
+        
+
+
         private void btnCreate_Click(object sender, EventArgs e)
         {
+            RegistryKey currentuserkey;
+            RegistryKey myKey;
+
             if (txtBox.Text != null)
             {
-                RegistryKey currentuserkey = Registry.CurrentUser;
-                RegistryKey myKey = currentuserkey.CreateSubKey(txtBox.Text);
-                myKey.SetValue(txtBoxName.Text, txtBoxValue.Text);
-                txtBox.Text = null;
-                txtBoxName.Text = null;
+                switch (cmbBoxRegistryList.SelectedIndex)
+                {
+                    case 0:
+                        currentuserkey = Registry.ClassesRoot;
+                        myKey = currentuserkey.CreateSubKey(txtBox.Text);
+                        
+                        break;
+                    case 1:
+                        currentuserkey = Registry.CurrentUser;
+                        myKey = currentuserkey.CreateSubKey(txtBox.Text);
+                        //MessageBox.Show((string)Registry.GetValue(cmbBoxRegistryList.SelectedItem.ToString(), txtBoxValue.Text, ""));
+                        break;
+                    case 2:
+                        currentuserkey = Registry.LocalMachine;
+                        myKey = currentuserkey.CreateSubKey(txtBox.Text);
+                        break;
+                    case 3:
+                        currentuserkey = Registry.Users;
+                        myKey = currentuserkey.CreateSubKey(txtBox.Text);
+                        break;
+                    case 4:
+                        currentuserkey = Registry.CurrentConfig;
+                        myKey = currentuserkey.CreateSubKey(txtBox.Text);
+                        break;
+                    default:
+                        MessageBox.Show("Ничего нет");
+                        break;
+                }
+
+                
+                    //RegistryKey currentuserkey = Registry.CurrentUser;
+                    //RegistryKey myKey = currentuserkey.CreateSubKey(txtBox.Text);
+                    //myKey.SetValue(txtBoxName.Text, txtBoxValue.Text);
+                    //txtBox.Text = null;
+                    //txtBoxName.Text = null;
                 txtBoxValue.Text = null;
 
+                
             }            
                 
+        }
+
+        private void cmbBoxRegistryList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnCreate.Enabled = true;
+            btnDelete.Enabled = true;
+            txtBox.Enabled = true;
+            txtBoxName.Enabled = true;
+            txtBoxValue.Enabled = true;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
